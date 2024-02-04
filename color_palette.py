3d061b7673e77c44dd55eac78983bea03c7c8704
Angel Talavera <ata@loriot.io>
2024-02-04 11:53:30 +0100

    Colors generation implemented

--- color_palette.py
+++ color_palette.py
@@ -2,4 +2,13 @@
 
 import colorgram
 
-colors = colorgram.extract("image", 15)
+
+def get_colors(image): 
+  colors = colorgram.extract(image, 15)
+  rgb_colors = []
+
+  for color in colors:
+    dec_color = (color.rgb.r, color.rgb.g, color.rgb.b)
+    rgb_colors.append(dec_color)
+    
+    return rgb_colors
